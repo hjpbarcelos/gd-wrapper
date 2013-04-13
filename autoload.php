904ef9bba035bda513d3b058e8d63d21823b57f2
Henrique Barcelos <rick.hjpbarcelos@gmail.com>
2013-04-13 22:28:35 +0200

    Changes in autoload.

--- autoload.php
+++ autoload.php
@@ -1,10 +1,11 @@
 <?php
 spl_autoload_register(function($className){
-	$path = __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR 
-		  . str_replace(
-				'\\', DIRECTORY_SEPARATOR, $className
-			) . '.php';
-	if(is_file($path)) {
+	$ds = DIRECTORY_SEPARATOR;	
+	$path = stream_resolve_include_path(
+		'src' . $ds . str_replace('\\', $ds, $className) . '.php'
+	);
+	
+	if($path !== false) {
 		require_once $path;
 	}
 });
