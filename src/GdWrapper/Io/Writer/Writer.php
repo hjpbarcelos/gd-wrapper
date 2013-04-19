9f0e8e5963744cb94894e251c2c6dbb72a2bf2fd
Henrique Barcelos <rick.hjpbarcelos@gmail.com>
2013-04-19 15:58:36 +0200

    Removes assets folder from root directory

--- src/GdWrapper/Io/Writer/Writer.php
+++ src/GdWrapper/Io/Writer/Writer.php
@@ -15,50 +15,50 @@ use \GdWrapper\Io\Preset;
  */
 interface Writer
 {
-   /**
-    * This parameter should be used with `write` method to
-    * indicate that the destination of output is the standard output.
-    *
-    * @var null
-    */
+    /**
+     * This parameter should be used with `write` method to
+     * indicate that the destination of output is the standard output.
+     *
+     * @var null
+     */
     const STDOUT = null;
-    
-	/**
-	 * Provides an output to an image resource.
-	 *
-	 * If you want to output to standar output, `$pathName` parameter
-	 * should be `STDOUT` class constant of this interface.
-	 *
-	 * @param string $pathName A path where to save the resource.
-	 * @param int $quality (optional) The quality of generated image.
-	 * 		Its value MUST be in a range from 0 to 100.
-	 * @param mixed $_ (optional) Additional parameters passed to
-	 * 		the concrete implementation method.
-	 *
-	 * @return void
-	 *
-	 * @throws Exception If cannot write the contents on file system.
-	 *
-	 * @see \GdWrapper\Io\Preset
-	 */
-	public function write(
-		$pathName,
-		$quality = Preset::IMAGE_QUALITY_MAX,
-		$_ = null
-	);
-	
-	/**
-	 * Obtains the Resource instance in which this object is working on.
-	 *
-	 * @return \GdWrapper\Resource\Resource
-	 */
-	public function getResource();
-	
-	/**
-	 * Obtains the Resource instance in which this object is working on.
-	 *
-	 * @param \GdWrapper\Resource\Resource $resource
-	 * @return void
-	 */
-	public function setResource(Resource $resource);
+
+    /**
+     * Provides an output to an image resource.
+     *
+     * If you want to output to standar output, `$pathName` parameter
+     * should be `STDOUT` class constant of this interface.
+     *
+     * @param string $pathName A path where to save the resource.
+     * @param int $quality (optional) The quality of generated image.
+     * 		Its value MUST be in a range from 0 to 100.
+     * @param mixed $_ (optional) Additional parameters passed to
+     * 		the concrete implementation method.
+     *
+     * @return void
+     *
+     * @throws Exception If cannot write the contents on file system.
+     *
+     * @see \GdWrapper\Io\Preset
+     */
+    public function write(
+        $pathName,
+        $quality = Preset::IMAGE_QUALITY_MAX,
+        $_ = null
+    );
+
+    /**
+     * Obtains the Resource instance in which this object is working on.
+     *
+     * @return \GdWrapper\Resource\Resource
+     */
+    public function getResource();
+
+    /**
+     * Obtains the Resource instance in which this object is working on.
+     *
+     * @param \GdWrapper\Resource\Resource $resource
+     * @return void
+     */
+    public function setResource(Resource $resource);
 }
